0eedc40f9aaf0f78fb4813e1a9ed8495a171813a
Abdlrhman Hegazy <abdlrhman.hegazy@yahoo.com>
2023-07-23 15:14:21 -0700

    Add validation for addTask,updateTask

--- src/modules/task/taskValidation.js
+++ src/modules/task/taskValidation.js
@@ -1,20 +1,36 @@
 import joi from "joi";
 
-export const addTask = joi
-  .object({
-    title: joi.string().min(3).max(25).required(),
-    description: joi.string().min(3).max(250).required(),
-    deadline: joi.string().min(10).max(10).required(),
-    assignTo: joi.string().min(24).max(24).required(),
-  })
-  .required();
+export const addTask = {
+  body: joi
+    .object({
+      title: joi.string().min(3).max(25).required(),
+      description: joi.string().min(3).max(250).required(),
+      deadline: joi
+        .date()
+        .options({ convert: false })
+        .greater("now")
+        .min(10)
+        .max(10)
+        .required(),
+      assignTo: joi.string().min(24).max(24).required(),
+    })
+    .required(),
+};
 
-export const updateTask = joi
-  .object({
-    title: joi.string().min(3).max(25).required(),
-    description: joi.string().min(3).max(250).required(),
-    deadline: joi.string().min(10).max(10).required(),
-    assignTo: joi.string().min(24).max(24).required(),
-    status: joi.string().required(),
-  })
-  .required();
+export const updateTask = {
+  body: joi
+    .object({
+      title: joi.string().min(3).max(25).required(),
+      description: joi.string().min(3).max(250).required(),
+      deadline: joi
+        .date()
+        .options({ convert: false })
+        .greater("now")
+        .min(10)
+        .max(10)
+        .required(),
+      assignTo: joi.string().min(24).max(24).required(),
+      status: joi.string().required(),
+    })
+    .required(),
+};
